b19dc8108806de3d8364cbf340c8d29efd24cf1e
Alyssa Cuson <acuson98@gmail.com>
2022-02-11 17:51:15 -0500

    added all GET routes for pages

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -5,6 +5,11 @@ const router = require('express').Router();
 router.get('/', async(req, res) =>{
     res.render('index')
 })
+
+// GET create user page
+router.get('/create-user', async(req, res) =>{
+    res.render('_create-user')
+})
 // GET dashboard
 // for now you can copy this rout and replace dashboard with your page name to check if it renders
 router.get('/dashboard', async(req, res) =>{
@@ -12,7 +17,9 @@ router.get('/dashboard', async(req, res) =>{
 })
 
 // GET your book clubs page
-
+router.get('/your-clubs', async(req, res) =>{
+    res.render('_your-clubs')
+})
 // GET explore book clubs page
 
 router.get('/explore', async(req, res) =>{
@@ -20,5 +27,7 @@ router.get('/explore', async(req, res) =>{
 })
 
 // GET create book clubs page
-
+router.get('/create-club', async(req, res) =>{
+    res.render('_create-club')
+})
 module.exports = router;
