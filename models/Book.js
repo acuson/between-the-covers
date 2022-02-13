bf0eaf279015368c3d053187b4b4d20f504549c1
Kevin Lacson <lacsonky@gmail.com>
2022-02-13 04:03:31 -0500

    base book model

---
+++ models/Book.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Book extends Model {}
+
+Book.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    google_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      types: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    thumbnail: {
+      type: DataTypes.STRING,
+    },
+    link: {
+      type: DataTypes.STRING,
+    },
+    club_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'club',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'book',
+  }
+);
