59e531bc1e5bc679b9093dc58eeca8812dda4cef
Kevin Lacson <lacsonky@gmail.com>
2022-02-14 15:04:10 -0500

    user get all, get by pk, and create user

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,7 +6,30 @@ const { User, Club, Book, Tag, User_Club } = require("../../models");
 //GET /api/users
 router.get("/", async (req, res) => {
     try {
-        // do stuff
+        const allUsers = await User.findAll({
+            attributes: { exclude: ["password"] },
+        });
+        res.status(200).json(allUsers);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+// desc: get user by primary key
+// GET /api/users/:id
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id, {
+            attributes: {
+                exclude: ["password"],
+            },
+        });
+        user == null
+            ? res
+                  .status(404)
+                  .json({ message: `User with id ${req.params.id} not found` })
+            : res.status(200).json(user);
     } catch (err) {
         console.error(err);
         res.status(500).json(err);
@@ -17,7 +40,18 @@ router.get("/", async (req, res) => {
 // POST /api/users/
 router.post("/", async (req, res) => {
     try {
-        // do stuff
+        const userInfo = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            username: req.body.user,
+            email: req.body.email,
+            password: req.body.password,
+        };
+        const response = await User.create(userInfo);
+
+        !response.ok
+            ? res.status(500).json({ message: "User creation failed" })
+            : res.status(200).json({ message: "User Created" });
     } catch (err) {
         console.error(err);
         res.status(500).json(err);
