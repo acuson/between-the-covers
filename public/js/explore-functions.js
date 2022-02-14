3f29d142a8a2a6126c8df04a9c03ce1d1cafcb7e
Christine Nguyen <cnhnguyen59@gmail.com>
2022-02-14 15:52:02 -0500

    created like function

--- public/js/explore-functions.js
+++ public/js/explore-functions.js
@@ -1,8 +1,5 @@
-/* let likeBtn = $('.like')
-let dislikeBtn = $('.dislike')
- */
-
-let text = 'hi'
+const router = require('express').Router();
+const {User, Club, Tag, User_Club } = require("../models");
 
 $('.dislike').on('click', dislike)
 
@@ -16,5 +13,27 @@ function dislike(e){
 }
 
 function like(e){
+    const card = e.target.parentElement.parentElement
+    const clubId = card.dataset.clubId
+
+    //GET USER ID and CLUB ID
+    //CREATE BOJECT
+    //EXPORT OBJECT OUT to router page
+
+    /* router.post('/', async (req,res) =>{
+        try{
+            const userData = await User.create({
+                user_id: 1,
+                club_id: `${clubId}`
+            });
+            res.status(200).json(userData);
+        } catch(err){
+            res.status(400).json(err)
+        }
+    }) */
+
+    card.style.visibility = 'hidden'
+    card.style.opacity = 0;
+    card.style.transition= 'opacity 1s';
 
 }
