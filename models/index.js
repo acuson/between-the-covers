8d7a16257dee23d6fd7c585efeaad2377f7789f0
Kevin Lacson <lacsonky@gmail.com>
2022-02-14 21:14:15 -0500

    fix: association club book

--- models/index.js
+++ models/index.js
@@ -15,13 +15,13 @@ Tag.belongsTo(Club, {
 //   foreignKey: 'club_id',
 // });
 
-// Club.hasOne(Book, {
-//   foreignKey: 'book_id',
-// });
+Club.hasOne(Book, {
+    foreignKey: "book_id",
+});
 
-// Book.belongsTo(Club, {
-//   foreignKey: 'book_id',
-// });
+Book.belongsTo(Club, {
+    foreignKey: "book_id",
+});
 
 Club.belongsToMany(User, { through: User_Club });
 User.belongsToMany(Club, { through: User_Club });
