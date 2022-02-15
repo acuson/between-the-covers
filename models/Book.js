33836159fe7b3d8a033a72bf1d8cc3939b79a6cc
Christine Nguyen <cnhnguyen59@gmail.com>
2022-02-14 21:27:54 -0500

    fixing join club post route

--- models/Book.js
+++ models/Book.js
@@ -4,55 +4,6 @@ const sequelize = require("../config/connection");
 class Book extends Model {}
 
 Book.init(
-<<<<<<< HEAD
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      allowNull: false,
-      primaryKey: true,
-    },
-    google_id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    author: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.TEXT,
-    },
-    thumbnail: {
-      type: DataTypes.STRING,
-    },
-    link: {
-      type: DataTypes.STRING,
-    },
-    publisher: {
-      type: DataTypes.STRING,
-    },
-    club_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'club',
-        key: 'id',
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'book',
-  }
-=======
     {
         id: {
             type: DataTypes.INTEGER,
@@ -100,7 +51,6 @@ Book.init(
         underscored: true,
         modelName: "book",
     }
->>>>>>> 19c2061b9bd79dd952be05d3bef147add3af85df
 );
 
 module.exports = Book;
--- server.js
+++ server.js
@@ -4,14 +4,8 @@ const path = require("path");
 const express = require("express");
 //const session = require("express-session");
 
-<<<<<<< HEAD
-const sequelize = require('./config/connection');
-const Models = require('./models');
-
-=======
 const sequelize = require("./config/connection");
 const Models = require("./models");
->>>>>>> 19c2061b9bd79dd952be05d3bef147add3af85df
 /* const SequelizeStore = require("connect-session-sequelize")(session.Store); */
 //Import and require mysql2
 const mysql = require("mysql2");
@@ -40,12 +34,7 @@ app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-<<<<<<< HEAD
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/jquery', express.static(__dirname, + '/node_modules/jquery/dist/'));
-=======
-app.use(express.static(path.join(__dirname, "public")));
->>>>>>> 19c2061b9bd79dd952be05d3bef147add3af85df
 
 app.set("layout", "/views/your-clubs");
 const routes = require("./routes");
