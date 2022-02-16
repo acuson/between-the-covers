4abc25cd12401fc37a5dae72d043f9d5120a959d
Alyssa Cuson <acuson98@gmail.com>
2022-02-16 14:03:39 -0500

    finished signup logic

--- public/js/login.js
+++ public/js/login.js
@@ -18,3 +18,35 @@ const loginFormHandler = async (event) => {
         }
     }
 };
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const fname = document.querySelector('#fname-signup').value.trim();
+    const lname = document.querySelector('#lname-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (fname && lname && email && password) {
+        const response = await fetch('/api/users', {
+          method: 'POST',
+          body: JSON.stringify({ fname, lname, email, password }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+    
+        if (response.ok) {
+          document.location.replace('/dashboard');
+        } else {
+          alert(response.statusText);
+        }
+      }
+    };
+    
+    document
+      .querySelector('.login-form')
+      .addEventListener('submit', loginFormHandler);
+    
+    document
+      .querySelector('.signup-form')
+      .addEventListener('submit', signupFormHandler);
+    
