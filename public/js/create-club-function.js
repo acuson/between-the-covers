26fd8855934749ab39ccc6a630719f1654f0e241
Darby Johnson <darbyjjohnson1@gmail.com>
2022-02-16 11:54:44 -0500

    saving

--- public/js/create-club-function.js
+++ public/js/create-club-function.js
@@ -1,34 +1,34 @@
-let createClub = {
- 
-    // name: req.body.name,
-    // description: req.body.description,
-    // club_book: req.body.club_book,
-    // /* book_id: req.body.book_id, */
-    // capacity: req.body.capacity,
-    // meeting_day: req.body.meeting_day,
-    // meeting_time: req.body.meeting_time,
-    // meeting_link: req.body.meeting_link,
-    // active: req.body.active,
+let nameEl = document.getElementById('name');
+let submitEl = document.getElementById('submit');
+let descriptionEl = document.getElementById('description');
+let club_bookEl = document.getElementById('club_book');
+let meeting_linkEl = document.getElementById('meeting_link');
+let meeting_timeEl = document.getElementById('meeting_time');
 
-    name: 'darby',
-    description: 'darby',
-    club_book: 'darby',
-    /* book_id: req.body.book_id, */
-    capacity: 5,
-    meeting_day: 'darby',
-    meeting_time: 'darby',
-    meeting_link: 'darby',
-    active: true,
+function submitBtn(event){
+    event.preventDefault();
+    let createClub = {
+        name: nameEl.value,
+        description: descriptionEl.value,
+        club_book: club_bookEl.value,
+        /* book_id: req.body.book_id, */
+        capacity: 5,
+        meeting_day: 'darby',
+        meeting_time: 'darby',
+        meeting_link: meeting_linkEl.value,
+        active: true,
+    };
+    const options ={
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+        },
+        body:JSON.stringify(createClub)
+    }
+    
+    fetch('/api/clubs/', options)
+    .then(data => console.log(data.json))
+    .catch(err => console.log(err))
 };
 
-const options ={
-    method: 'POST',
-    headers:{
-        'Content-Type': 'application/json',
-    },
-    body:JSON.stringify(createClub)
-}
-
-fetch('/api/clubs/', options)
-.then(data => console.log(data.json))
-.catch(err => console.log(err))
+submitEl.addEventListener("click", submitBtn);
