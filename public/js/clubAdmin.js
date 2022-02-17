48b201d1c8884b85e454a4ab11eab969c7d91e16
Kevin Lacson <lacsonky@gmail.com>
2022-02-17 11:14:02 -0500

    added comments to clubAdmin.js

--- public/js/clubAdmin.js
+++ public/js/clubAdmin.js
@@ -7,26 +7,36 @@ let clubLink = document.querySelector("#club-link");
 
 let btns = document.querySelectorAll("button");
 
+// PUT request to api to update club
 const updateClub = async (key, value) => {
     let reqBody = {};
+    // Assign parameters as object
     reqBody[key] = value;
+    // Pull Club id from url string that was passed from previous page
     const urlString = window.location.search;
     const clubId = new URLSearchParams(urlString).get("id");
-    console.log(clubId);
-    console.log(reqBody);
-    await fetch(`/api/clubs/${clubId}`, {
-        method: "PUT",
-        body: JSON.stringify(reqBody),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
+
+    // Fetch request to API
+    try {
+        await fetch(`/api/clubs/${clubId}`, {
+            method: "PUT",
+            body: JSON.stringify(reqBody),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        console.log(`Club with id ${clubId} has been updated`);
+    } catch (err) {
+        console.log(err);
+        console.log("Club not updated");
+    }
 };
 
-const click = e => {
+// Toggle textarea enabled/disabled and save
+const editData = e => {
     e.preventDefault();
+    // Selects textarea sibling
     let currentBtn = e.currentTarget.previousElementSibling;
-    // console.log(currentBtn.getAttribute("data-active"));
     if (currentBtn.getAttribute("data-active") == "false") {
         currentBtn.removeAttribute("disabled");
         currentBtn.setAttribute("data-active", true);
@@ -35,10 +45,12 @@ const click = e => {
         currentBtn.setAttribute("data-active", false);
         const key = currentBtn.getAttribute("id");
         const value = currentBtn.value;
+        // Calls function to update club, passing in key from id selector as param
         updateClub(key, value);
     }
 };
 
+// Event listener for each button
 btns.forEach(btn => {
-    btn.addEventListener("click", click);
+    btn.addEventListener("click", editData);
 });
